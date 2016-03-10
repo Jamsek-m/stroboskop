dd2f2c182ea9b75025a90d7ec6251149240dfbd7
Miha Jamsek <miha_jamsek@windowslive.com>
2016-03-10 10:21:33 +0000

    Dinamika gumba za ustavitev stroboskopa

--- skripta.js
+++ skripta.js
@@ -32,10 +32,15 @@ window.addEventListener('load', function() {
 		}		
 	}
 	
+	
 	var stop = function(event) {
 		ustavi = true;
+		var start = document.querySelector("#start");
+		start.innerHTML = "Zazeni stroboskop";
+		start.removeEventListener('click', stop);
+		start.addEventListener('click', zagon);
 	}
-	
+
 	var zagon = function(event) {
 		vrednosti = [];
 		var barve = document.querySelectorAll("#barve > button");
